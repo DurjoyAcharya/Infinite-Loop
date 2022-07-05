0dec92157df91746116d89a54b557c700e708d6d
Durjoy <da-durjoy@outlook.com>
2022-07-05 12:20:16 +0600

    start with angular proj

--- Loop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def index2(request):
-    return render(request,'index2.html')
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn InfiniteLoop.wsgi --log-file -
