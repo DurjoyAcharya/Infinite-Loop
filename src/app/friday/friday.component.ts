7a9cc01f04d36207605c268021c1d2e94d4028f4
Durjoy <da-durjoy@outlook.com>
2022-07-23 01:51:27 +0600

    print rest data

--- src/app/friday/friday.component.ts
+++ src/app/friday/friday.component.ts
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friday.component.css'],
 })
 export class FridayComponent implements OnInit {
+  users: any;
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
       .get('https://jsonplaceholder.typicode.com/users')
       .subscribe((data) => {
-        console.log(data);
+        this.users = data;
       });
   }
 }
